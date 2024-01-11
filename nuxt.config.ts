a5ca41498956882e191adf01d8def971a889c8de
kyung <bamtori1029@gmail.com>
2024-01-11 22:35:12 +0900

    메타데이터 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,17 @@ export default {
   css: ["@/assets/css/reset.css"],
   modules: ["@pinia/nuxt"],
   target: "static",
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: "learn-nuxt",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
 };
