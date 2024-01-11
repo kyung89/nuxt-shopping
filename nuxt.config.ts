9f823ffca1af37abc65314b9d99527059b890389
kyung <bamtori1029@gmail.com>
2024-01-11 22:59:54 +0900

    nuxt3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default {
+export default defineNuxtConfig({
   devtools: { enabled: true },
   devServer: {
     port: 8000,
   },
   css: ["@/assets/css/reset.css"],
   modules: ["@pinia/nuxt"],
-  target: "static",
+  ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   app: {
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+      title: "My Nuxt Shopping App",
+      meta: [{ name: "description", content: "My amazing site" }],
     },
   },
-};
+});
