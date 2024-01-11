d12cd85a4e6668c12d3aa4ab0f299798f4b11210
kyung <bamtori1029@gmail.com>
2024-01-11 22:37:51 +0900

    메타 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,16 +8,10 @@ export default {
   modules: ["@pinia/nuxt"],
   target: "static",
   // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: "learn-nuxt",
-    htmlAttrs: {
-      lang: "en",
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
     },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 };
